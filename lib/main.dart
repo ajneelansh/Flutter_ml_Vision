e9d56ad09488d307635cf2cae39fced81c93587e
Neelansh Jaiswal <119448065+ajneelansh@users.noreply.github.com>
2023-11-11 00:35:17 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import 'pages/facePage.dart';
 import 'stateModel/appstate.dart';
 
@@ -19,7 +18,6 @@ void logError(String code, String message) =>
     print('Error: $code\nError Message: $message');
 class MyApp extends StatelessWidget {
 final  AppState _appState = AppState();
-
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<AppState>(
